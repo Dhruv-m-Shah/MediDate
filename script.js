460cfbb183dc23b3751bd2fa201a732cba06b9f6
Allen <allenlu@live.ca>
2020-02-01 23:53:40 -0500

    second update to AJAX pills_update

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // make a function that updates the pill numbers (innerHTML) when there is an update (from AJAX)
 
 function update_pills() {
+    var response = resp.data;
+    
+    for (drug of response) {
+        if (drug.Red != 0) {
+            document.getElementById("morning").innerHTML.concat(document.getElementById("morning").innerHTML.concat, "")
+        }
+    }
 
+
+    
     
 }
